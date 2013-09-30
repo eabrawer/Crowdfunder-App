76ed1fd66cdafb654eadfeae54b5daabd14acd50
eabrawer <ericbrawer@gmail.com>
2013-09-30 13:36:24 -0400

    Disabling fixtures for rspec

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ end
 
 module Crowdfunder
   class Application < Rails::Application
+    config.generators do |g|
+      g.test_framework :rspec, :fixture => false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
