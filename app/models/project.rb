d9ee114a83b4d53f647cd6e44adaed67aec9e9e3
eabrawer <ericbrawer@gmail.com>
2013-09-30 13:50:18 -0400

    Generating Project model with validations

---
+++ app/models/project.rb
@@ -0,0 +1,8 @@
+class Project < ActiveRecord::Base
+  attr_accessible :description, :goal, :teaser, :title, :user_id
+  belongs_to :user
+  validates :title, presence: true, length: {maximum: 100}
+  validates :teaser, presence: true, length: {maximum: 255}
+  validates :description, presence: true
+  validates :goal, presence: true, numericality: {greater_than: 0, allow_blank: true}
+end
