b2d8ccf09a5b296d5acb0741a4a93a7eb211aa7a
eabrawer <ericbrawer@gmail.com>
2013-10-03 16:36:20 -0400

    Generating factory for pledge.

--- spec/factories/pledges.rb
+++ spec/factories/pledges.rb
@@ -2,8 +2,8 @@
 
 FactoryGirl.define do
   factory :pledge do
-    user_id 1
-    project_id 1
-    amount 1
+    user
+    project
+    amount 100
   end
 end
--- test/integration/project_flows_test.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-class ProjectFlowsTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
